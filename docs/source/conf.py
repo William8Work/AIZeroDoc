8dbbe2d05256e6b4a03cba3649b02cceb2ab24cb
William Li <william8work@gmail.com>
2020-03-15 16:47:08 -0700

    set master doc to index.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'AIZero'
-copyright = '2020, William Li'
-author = 'William Li'
+copyright = '2020, AIZero.org'
+author = 'AIZero.org'
 
 
 # -- General configuration ---------------------------------------------------
@@ -50,3 +50,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+master_doc = 'index'
+
